5b90bd2ad4dc963ecc2d775bbec4385eb32cbd68
aman7github <amanmaurya9044@gmail.com>
2023-06-01 22:36:28 +0530

    address route single user address add

--- model/Address.model.js
+++ model/Address.model.js
@@ -2,12 +2,12 @@
 const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
-    "name":{type:String,required:true},
-    "mobile":{type:Number,required:true},
-    "pincode":{type:Number,required:true},
-    "city":{type:String,required:true},
-    "building":{type:String,required:true},
-    "street":{type:String,required:true},
+    "name":String,
+    "mobile":Number,
+    "pincode":Number,
+    "city":String,
+    "building":String,
+    "street":String,
     "landmark":String,
     "userID":String
 },{
